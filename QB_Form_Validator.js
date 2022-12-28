ebf3b456fac54cc67deb7f4f9f3ed9d20ba97775
John Hewitt <johnh@synctivate.com>
2022-12-28 12:40:36 -0500

    additional changes

--- QB_Form_Validator.js
+++ QB_Form_Validator.js
@@ -23,6 +23,24 @@ fields = [
     }
 ]
 
+function get_form_data(form_class) {
+    let form = document.querySelector(`.${form_class}`);
+    let fields = form.querySelectorAll('.fieldset');
+    let fieldArray = [];
+    fields.forEach(field => {
+        let input = field.querySelector('.field-input');
+        let label = field.querySelector('.field-label');
+        let fieldObj = {
+            name: input.id,
+            value: input.value,
+            required: input.required,
+            type: input.type,
+        }
+        fieldArray.push(fieldObj);
+    })
+    return fieldArray;
+}
+
 async function validate(fields){
     // loop through each field
     for (let i = 0; i < fields.length; i++){
@@ -34,49 +52,49 @@ async function validate(fields){
                 return false;
             }
         }
-        // check if field is text
+        // check if field is text or blank
         if (fields[i].type === "text"){
             if (!isText(fields[i].value)){
                 alert(fields[i].name + " must be text");
                 return false;
             }
         }
-        // check if field is multiple choice
+        // check if field is multiple choice or blank
         if (fields[i].type === "multipleChoice"){
             if (!isMultipleChoice(fields[i].value, fields[i].options)){
                 alert(fields[i].name + " must be one of the following option: " + fields[i].options.join(", "));
                 return false;
             }
         }
-        // check if field is email
+        // check if field is email or blank
         if (fields[i].type === "email"){
             if (!isEmail(fields[i].value)){
                 alert(fields[i].name + " must be a valid email");
                 return false;
             }
         }
-        // check if field is number
+        // check if field is number or blank
         if (fields[i].type === "number"){
             if (!isNumber(fields[i].value)){
                 alert(fields[i].name + " must be a number");
                 return false;
             }
         }
-        // check if field is date
+        // check if field is date or blank
         if (fields[i].type === "date"){
             if (!isDate(fields[i].value)){
                 alert(fields[i].name + " must be a date");
                 return false;
             }
         }
-        // check if field is boolean
+        // check if field is boolean or blank
         if (fields[i].type === "boolean"){
             if (!isBoolean(fields[i].value)){
                 alert(fields[i].name + " must be a boolean");
                 return false;
             }
         }
-        // check if field is phone
+        // check if field is phone or blank
         if (fields[i].type === "phone"){
             if (!isPhone(fields[i].value)){
                 alert(fields[i].name + " must be a valid 10 digit phone number");
@@ -96,9 +114,9 @@ function isMultipleChoice(value, options){
     return options.includes(value);
 }
 
-// function to check if a value is an email
+// use regex to check if value is a valid email
 function isEmail(value){
-    return value.includes("@");
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
 }
 
 // function to check if a value is a number
